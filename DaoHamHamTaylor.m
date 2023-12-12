b10b17f663843e96e81d6b6f7603f0b360d23a8d
phong1239 <152763055+phong1239@users.noreply.github.com>
2023-12-12 20:51:15 +0700

    Add files via upload

--- DaoHamHamTaylor.m
+++ DaoHamHamTaylor.m
@@ -1,4 +1,26 @@
-function [dx] = DaoHamHamTaylor(fx,x,h,pp,hscc)
+function [dx]=DaoHamHamTaylor(xi,yi,fx,h,x,ct,pp,hscc)
+if ct=="xy"
+    h=xi(2)-xi(1);
+    n=(x-xi(1))/h +1;
+    n=cast(n,'uint16');
+    if hscc == "O(h^2)"
+        if pp=="Tiến"
+            dx=(4*yi(n+1)-3*yi(n)-yi(n+2))/(2*h);
+        elseif pp=="Lùi"
+            dx=((3*yi(n)-4*yi(n-1)+yi(n-2))/(2*h));
+       else 
+            dx = (yi(n+1)-yi(n-1))/(2*h);
+        end
+    else
+        if pp=="Tiến"
+            dx=(yi(n+1)-yi(n))/(h);
+        elseif pp=="Lùi"
+            dx=(yi(n)-yi(n-1))/(h);
+        else 
+            dx = 0;
+        end
+    end
+else
     fx=str2func(['@(x)',fx]);
     if hscc == "O(h^2)"
         if pp=="Tiến"
